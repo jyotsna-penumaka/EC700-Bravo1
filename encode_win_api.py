1e22d01d381e39013d3f8c341599bbf1823d57de
jyotsna-penumaka <jyotsnap@bu.edu>
2021-02-13 14:25:35 -0500

    Encode the API call traces and save it in a file

--- encode_win_api.py
+++ encode_win_api.py
@@ -2,7 +2,6 @@ import csv
 import pandas as pd 
 from IPython.display import display 
 import json
-import os.path
 from os import path
 from zipfile import ZipFile
 
@@ -13,8 +12,6 @@ df = pd.read_csv('malware_dataset/malware_API_dataset.csv',header=None,sep='\n')
 df = df[0].str.split(',', expand=True)
 rows = df.shape[0] 
 cols = df.shape[1] 
-print("rows: " + str(rows))
-print("cols: " + str(cols))
 
 if path.exists("apiDict.json"):
     with open('apiDict.json') as json_file: 
@@ -31,7 +28,6 @@ for i in range (0,rows):
                 last_key = list(apiDict.keys())[-1]
                 apiDict[val] = apiDict[last_key]+1
 
-print(apiDict)
 json = json.dumps(apiDict)
 f = open("apiDict.json","w")
 f.write(json)
