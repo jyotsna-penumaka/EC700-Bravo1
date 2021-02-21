dceb7f500818c0a98edae5f4ae53c93739769738
jyotsna-penumaka <jyotsnap@bu.edu>
2021-02-21 15:54:08 -0500

    Extract API Call Traces after executing a malware executable in CUCKOO

---
+++ API_Calls/extract_api_calls.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+with open("report.json", "r") as read_file:
+    data = json.load(read_file)
+    calls = data.get("behavior").get("processes")[1].get("calls");
+    numberOfCalls = len(calls)
+    api_calls = []
+    for i in range (0,numberOfCalls) :
+        api_calls.append(calls[i].get("api"))
+
+    with open('cuckoo_calls.csv','w') as file:
+        writer = csv.writer(file)
+        writer.writerow(api_calls)
+
+    if path.exists("apiDict.json"):
+        with open('apiDict.json') as json_file: 
+        apiDict = json.load(json_file) 
